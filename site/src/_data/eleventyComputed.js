f9efa4f6c2b5cf105650dfb121b4cb6cdce8a239
auric <aloning@gmail.com>
2026-07-06 00:43:41 +0800

    auto-implement refs #248: Remove the legacy frontmatter post contract bridge

--- site/src/_data/eleventyComputed.js
+++ site/src/_data/eleventyComputed.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const path = require("node:path");
-const { postFromEleventyData } = require("../../lib/posts");
 const { pageLastUpdated } = require("../_includes/utils/last-updated");
 
 const SITE_ROOT = path.resolve(__dirname, "../..");
@@ -9,8 +8,5 @@ const SITE_ROOT = path.resolve(__dirname, "../..");
 module.exports = {
   lastUpdated(data) {
     return pageLastUpdated(data, { siteRoot: SITE_ROOT });
-  },
-  post(data) {
-    return postFromEleventyData(data);
   },
 };
